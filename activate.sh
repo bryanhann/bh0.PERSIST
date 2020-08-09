4631f152c1bd8646c464d0d6e81e75b5ea850d73
Bryan Hann <nobody@nowhere>
2020-08-09 14:10:57 +0800

    Initial commit.

---
+++ activate.sh
@@ -0,0 +1,9 @@
+export BH0SHELL=$(dirname $0)
+bh0export HISTFILE    $(dirname $0)/HISTFILE
+bh0export HISTSIZE    5000            # How many lines of history to keep in memory
+bh0export SAVEHIST    5000            # Number of history entries to save to disk
+bh0export HISTDUP     erase           # Erase duplicates in the history file
+bh0exec setopt appendhistory          # Append history to the history file (no overwriting)
+bh0exec setopt sharehistory           # Share history across terminals
+bh0exec setopt incappendhistory       # Immediately
+
