32a5e02d52c313fd6b8fcd36412b1441fa95ee50
Bryan Hann <nobody@nowhere>
2020-08-17 10:23:02 +0800

    Refactor. Tweak.

--- activate.sh
+++ activate.sh
@@ -1,4 +1,3 @@
-bh0include bh0.shell.persist
 bh0export HISTFILE    $(bh0module bh0.shell.persist)/HISTFILE
 bh0export HISTSIZE    5000            # How many lines of history to keep in memory
 bh0export SAVEHIST    5000            # Number of history entries to save to disk
